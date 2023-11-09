0b0ed32e799e24b3b69f023b19c98c9f1a2f80b4
Gary Liggins <GLiggins@kmbsus.local>
2023-11-09 14:56:55 -0800

    added build

---
+++ build/precache-manifest.6991ce2964e4cb4924fe3ff5d08751b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc86aaa8c58f636a5f9e",
+    "url": "/static/css/main.2f719820.chunk.css"
+  },
+  {
+    "revision": "bc86aaa8c58f636a5f9e",
+    "url": "/static/js/main.bc86aaa8.chunk.js"
+  },
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/static/js/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "4cc16d015539da4dc13b",
+    "url": "/static/css/2.c8f9c74d.chunk.css"
+  },
+  {
+    "revision": "4cc16d015539da4dc13b",
+    "url": "/static/js/2.4cc16d01.chunk.js"
+  },
+  {
+    "revision": "bed8864b9b0e4c81fc063b7fa395ee2f",
+    "url": "/index.html"
+  }
+];
